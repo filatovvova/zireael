f0f090a8872e2696bfe461e7fa8d79f34b9b8579
Vova <filatov.vova@gmail.com>
2021-12-01 00:29:37 +0300

    pampam

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import yaml
 import logging.config
+from zireael import *
+from configs import const
 
 
 logging.config.fileConfig('./configs/logger.config')
 logger = logging.getLogger('zireaelLogger')
 
-logger.debug('pampam')
+fields = """fill INTEGER\n"""
+with open(const.sqlite_conf_path, 'r') as file:
+    db_conf = yaml.safe_load(file)
 
-with open('./configs/sqlite.yaml', 'r') as file:
-    prime_service = yaml.safe_load(file)
-print(prime_service)
+conn = sqlite3.connect(db_conf['db']['test']['conn_string'])
 
-for row in prime_service['db']:
-    print(row)
+result = db_create_table(table_name='table_fill_3', fields=fields, db_type='sqlite', db_name='test')
+print(result)
 
--- sqlite_db.py
+++ sqlite_db.py
@@ -1,4 +1,59 @@
 import sqlite3
+import logging.config
+import yaml
+from configs import const
+
+logging.config.fileConfig('./configs/logger.config')
+logger = logging.getLogger('zireaelLogger')
+
 
 def sqlite_create_table(table_name, fields, db_name, conn):
-    if conn != 'none':
+    script = 'create table {}\n (\n  {} )'.format(table_name, fields)
+    if conn == 'none':
+        logger.info('start create table {} in sqlite db = {}'.format(table_name, db_name))
+        cur = None
+        conn = None
+        try:
+            with open(const.sqlite_conf_path, 'r') as file:
+                db_conf = yaml.safe_load(file)
+
+            conn = sqlite3.connect(db_conf['db'][db_name]['conn_string'])
+            cur = conn.cursor()
+
+            logger.debug('start execute table creation:\n{}'.format(script))
+            cur.execute(script)
+        except FileNotFoundError as er_message:
+            logger.error('Ups, error during table creation: {}'.format(er_message))
+            return -1
+        except sqlite3.Error as er_message:
+            logger.error('Ups, error during table creation: {}'.format(er_message))
+            return -1
+        else:
+            logger.debug('end execute table creation')
+        finally:
+            if cur:
+                cur.close()
+            if conn:
+                conn.close()
+        logger.info('end create table {} in sqlite db = {}'.format(table_name, db_name))
+        return 1
+
+    else:
+        logger.info('start create table {} sqlite db. Conn is sent to the function'.format(table_name))
+        cur = None
+        try:
+            cur = conn.cursor()
+
+            logger.debug('start execute table creation:\n{}'.format(script))
+            cur.execute(script)
+        except sqlite3.Error as er_message:
+            logger.error('Ups, error during table creation: {}'.format(er_message))
+            return -1
+        else:
+            logger.debug('end execute table creation')
+        finally:
+            if cur:
+                cur.close()
+        logger.info('end create table {} sqlite db. Conn is sent to the function'.format(table_name))
+        return 1
+
